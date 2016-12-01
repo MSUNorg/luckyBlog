c43ac090bcc977ead10ac7a0d63b47d7ff5ff856
张兄才 <zhangxiongcai337@gmail.com>
2016-12-01 19:18:57 +0800

    Update start_luckyBlog.sh

--- bin/start_luckyBlog.sh
+++ bin/start_luckyBlog.sh
@@ -43,7 +43,7 @@ JAVA_OPTS="-server -Xms2048m -Xmx2048m \
 -XX:+UseParNewGC \
 -XX:ParallelGCThreads=4 \
 -XX:+PrintGCDetails \
--Xloggc:/data/logs/msun/wxbet/jvm_gc.log"
+-Xloggc:/data/logs/msun/lucky/blog/jvm_gc.log"
 
 
 export JAVA_OPTS
@@ -52,4 +52,4 @@ echo "JAVA_HOME  :$JAVA_HOME"
 echo "SERVER_HOME:$SERVER_HOME"
 echo "CLASSPATH  :$CLASSPATH"
 
-exec $java $JAVA_OPTS com.msun.wxbet.WxbetApplication $* &
+exec $java $JAVA_OPTS com.msun.luckyBlog.Launcher $* &
